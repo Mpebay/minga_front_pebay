2ef248d8bf41647d7f41e108dbb2489af8a1df2e
Manuel Pebay <manupebay@hotmail.com>
2023-08-28 11:49:00 -0300

    Entrega M03-Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+
 import './App.css'
 import Body from './assets/Components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    
       <Body/>
   )
 }
